f243beb808791819c30758242cea6433a4c64c03
Michael Bumann <michael@derbumi.com>
2008-07-28 05:07:33 +0200

    initial commit

---
+++ campaign_monitor.rb
@@ -0,0 +1,52 @@
+require 'soap/wsdlDriver'
+
+class CampaignMonitor
+
+	@@api_key = ""
+  @@driver = SOAP::WSDLDriverFactory.new("http://app.campaignmonitor.com/api/api.asmx?WSDL").create_rpc_driver
+  
+  attr_accessor :list_id
+
+  def initialize(list_id)
+    @list_id = list_id
+  end
+  
+  def add_subscriber(args={})
+    name = args.delete(:name)
+    email = args.delete(:email)
+    custom_fields = []
+    args.each do |key,value|
+      custom_fields << {:Key => key, :Value => value}
+    end
+    
+    response = driver.AddAndResubscribeWithCustomFields \
+                     :ApiKey       => @@api_key, 
+                     :ListID       => list_id, 
+                     :Email        => "#{email}",
+                     :Name         => "#{name}",
+                     :CustomFields => {:SubscriberCustomField =>
+                       custom_fields
+                     }
+    driver.reset_stream
+    response.subscriber_AddAndResubscribeWithCustomFieldsWSDLResult
+  end
+
+  def remove_subscriber(email)
+    response = driver.Unsubscribe \
+                 :ApiKey       => API_KEY, 
+                 :ListID       => list_id, 
+                 :Email        => email
+    soap.reset_stream
+    response.subscriber_AddWithCustomFieldsWSDLResult
+  end
+  
+  def self.api_key=(value)
+    @@api_key = value
+  end
+  private
+
+    def driver
+      @@driver
+    end
+
+end
---
+++ readme.txt
@@ -0,0 +1,25 @@
+CAMPAIGNMONITOR
+=============
+
+This is a simple CampaignMonitor.com class to add/remove subscribers to/from subscriber lists.
+I'm using this to add and remove subscribers/users from my app to campaignmonitor.com
+
+USAGE:
+---------
+
+require "campaign_monitor"
+
+CampaignMonitor.api_key = "your key here"
+
+campaign_monitor = CampaignMonitor.new( [LIST_ID] )
+
+campaign_monitor.add_subscriber(:email => "me@spam.com", :name =>"Michael Spam") # note everything but email and name will be passed to CampaignMonitor as custom field
+
+
+campaign_monitor.remove_subscriber("me@spam.com")
+
+
+Credits:
+---------
+http://bjhess.com/bjhessblog/2008/02/19/calling-mailbuild-addwithcustomfields-with-ruby/
+Michael Bumann - railslove.com
